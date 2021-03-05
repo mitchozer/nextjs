386caf49d81fc01a8992cf9a22a32f5e79fcad87
mitchozer <mitchozer@users.noreply.github.com>
2021-03-05 00:39:28 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Start by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
